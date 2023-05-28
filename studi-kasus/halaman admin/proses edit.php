7755d6ebc2e4820c794ca193c7456e214254ff74
yulips <anayuliputri@gmail.com>
2023-05-28 12:40:41 +0700

    add file tugas studi-kasus individu

---
+++ studi-kasus/halaman admin/proses edit.php
@@ -0,0 +1,44 @@
+<?php 
+    include_once("db.php");
+
+    $isbn = $_POST["isbn"];
+
+    $judul = $_POST["judul"];
+    $tahun = $_POST["tahun"];
+    $id_penerbit = $_POST["id_penerbit"];
+    $id_pengarang = $_POST["id_pengarang"];
+    $id_kategori = $_POST["id_kategori"];
+    $id_jenisBuku = $_POST["id_jenisBuku"];
+    $stok = $_POST["stok"];
+    $harga_jual = $_POST["harga_jual"];
+    
+    if(isset($_FILES)){
+        $gambar = $_FILES["gambar"]["name"];
+        $source = $_FILES["gambar"]["tmp_name"];
+        $folder = 'img buku/';
+        $target_file = $folder. basename($_FILES["gambar"]["name"]);
+        $uploadOk = true;
+
+            move_uploaded_file($source, $target_file);
+    }
+       
+    $result = mysqli_query($conn, "UPDATE `buku` SET 
+                                    isbn = '$isbn',
+                                    judul = '$judul',
+                                    tahun = '$tahun',
+                                    id_pengarang = '$id_pengarang', 
+                                    id_penerbit = '$id_penerbit',
+                                    id_kategori = '$id_kategori', 
+                                    id_jenisBuku = '$id_jenisBuku',
+                                    stok = '$stok', 
+                                    harga_jual = '$harga_jual',
+                                    gambar = '$gambar'
+
+                                    
+                                    WHERE isbn = '$isbn'
+                                    ");
+
+
+    header("Location: admin.php");
+
+?>
---
+++ studi-kasus/halaman index/index.php
@@ -0,0 +1,392 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Bukukita</title>
+     
+      <!-- bootstrap css -->
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+      <!-- style css -->
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <link rel="stylesheet" type="text/css" href="style1.css">
+
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+
+      <!-- fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Great+Vibes|Open+Sans:400,700&display=swap&subset=latin-ext" rel="stylesheet">
+      
+      <!-- owl stylesheets --> 
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <link rel="stylesheet" href="css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+   
+      <!-- Feather Icons -->
+      <script src="https://unpkg.com/feather-icons"></script>
+
+      <script src="script.js"></script>
+      
+   </head>
+   <body>
+      <!-- Navbar Start -->
+      <nav id="navbar" class="navbar navbar-expand-lg navbar-light">
+         <div class="container-fluid">
+               <a class="navbar-brand" href="#">Buku<span>kita.</span></a>
+               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                     <li class="nav-item">
+                           <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                     </li>
+                     <li class="nav-item">
+                           <a class="nav-link" href="#about">About Us</a>
+                     </li>
+                     <li class="nav-item">
+                           <a class="nav-link" href="#buku">Kategori</a>
+                     </li>
+                     <li class="nav-item">
+                           <a class="nav-link" href="#kontak">Kontak</a>
+                     </li>
+                  </ul>
+
+                  <form class="d-flex">
+                  <a href="#" id="search"><i data-feather="search"></i></a>
+                  <a href="#" id="shopping"><i data-feather="shopping-cart"></i></a>
+                  <a href="../halaman admin/admin.php" id="user"><i data-feather="user"></i></a>
+                  </form>
+               </div>
+         </div>
+      </nav>
+      <!-- Navbar End -->
+
+      <!-- banner section start -->
+      <div class="banner_section layout_padding" style="margin-top:3rem;">
+         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <div class="container">
+                     <div class="row">
+                        <div class="col-sm-9">
+                           <img src="img/diskon2.png" alt="">
+                        </div>
+                        <div class="col-sm-3">
+                           <div class="banner_img"><img src="img/buku novel.jpg"></div>
+                           <div class="banner_img" style="margin-top:0.5rem;"><img src="img/buku anak.jpg"></div>
+                           <div class="banner_img" style="margin-top:0.5rem;"><img src="img/bestseller.jpg"></div>
+                           <div class="banner_img" style="margin-top:0.5rem;"><img src="img/buku pengambangan.jpg"></div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <div class="container">
+                     <div class="row">
+                        <div class="col-sm-6">
+                           <h1 class="banner_taital">Promo<br>Diskon</h1>
+                           <p class="banner_text">Nantikan promo diskon gebyar gedebuk dari kami, Stay Tune!<i data-feather="smile"></i></p>
+                           <div class="read_bt"><a href="#">Buy Now</a></div>
+                        </div>
+                        <div class="col-sm-6">
+                           <div class="banner_img"><img src="img/promo.jpeg"></div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <div class="container">
+                     <div class="row">
+                        <div class="col-sm-6">
+                           <h1 class="banner_taital">Promo <br>Diskon</h1>
+                           <p class="banner_text">Nantikan promo diskon gebyar gedebuk dari kami, Stay Tune!<i data-feather="smile"></i></p>
+                           <div class="read_bt"><a href="#">Buy Now</a></div>
+                        </div>
+                        <div class="col-sm-6">
+                           <div class="banner_img"><img src="img/promo2.jpg"></div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- banner section end -->
+
+      <!-- product section start -->
+      <div id="buku" class="product_section layout_padding">
+         <div class="container">
+            <div class="row">
+               <div class="col-sm-12">
+                  <h1 class="product_taital">Rekomendasi Buku</h1>
+               </div>
+            </div>
+            <div class="product_section_2 layout_padding">
+               <div class="row">
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="product_box">
+                        <h4 class="bursh_text">Dilan</h4>
+                        <img src="img/buku/dilan.jpg" class="image_1">
+                        <div class="btn_main">
+                           <div class="buy_bt">
+                              <ul>
+                                 <li><a href="#">Beli</a></li>
+                              </ul>
+                           </div>
+                           <h3 class="price_text">Rp 78.900</h3>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="product_box">
+                        <h4 class="bursh_text">Nebula</h4>
+                        <img src="img/buku/nebula.jpg" class="image_1">
+                        <div class="btn_main">
+                           <div class="buy_bt">
+                              <ul>
+                                 <li><a href="#">Beli</a></li>
+                              </ul>
+                           </div>
+                           <h3 class="price_text">Rp 79.900</h3>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="product_box">
+                        <h4 class="bursh_text">Milea</h4>
+                        <img src="img/buku/milea.jpg" class="image_1">
+                        <div class="btn_main">
+                           <div class="buy_bt">
+                              <ul>
+                                 <li><a href="#">Beli</a></li>
+                              </ul>
+                           </div>
+                           <h3 class="price_text">Rp 89.900</h3>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="product_box">
+                        <h4 class="bursh_text">Dewasa itu Luka?</h4>
+                        <img src="img/buku/dewasa.jpg" class="image_1">
+                        <div class="btn_main">
+                           <div class="buy_bt">
+                              <ul>
+                                 <li><a href="#">Beli</a></li>
+                              </ul>
+                           </div>
+                           <h3 class="price_text">Rp 129.900</h3>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="product_box">
+                        <h4 class="bursh_text">Tentang Tubuhmu</h4>
+                        <img src="img/buku/tentang tubuh.jpg" class="image_1">
+                        <div class="btn_main">
+                           <div class="buy_bt">
+                              <ul>
+                                 <li><a href="#">Beli</a></li>
+                              </ul>
+                           </div>
+                           <h3 class="price_text">Rp 49.900</h3>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="product_box">
+                        <h4 class="bursh_text">Rindu</h4>
+                        <img src="img/buku/rindu.jpeg" class="image_1">
+                        <div class="btn_main">
+                           <div class="buy_bt">
+                              <ul>
+                                 <li><a href="#">Beli</a></li>
+                              </ul>
+                           </div>
+                           <h3 class="price_text">Rp 99.900</h3>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="product_box">
+                        <h4 class="bursh_text">Detektif Conan</h4>
+                        <img src="img/buku/detektif.jpg" class="image_1">
+                        <div class="btn_main">
+                           <div class="buy_bt">
+                              <ul>
+                                 <li><a href="#">Beli</a></li>
+                              </ul>
+                           </div>
+                           <h3 class="price_text">Rp 75.000</h3>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="product_box">
+                        <h4 class="bursh_text">Hujan</h4>
+                        <img src="img/buku/hujan.jpg" class="image_1">
+                        <div class="btn_main">
+                           <div class="buy_bt">
+                              <ul>
+                                 <li><a href="#">Beli</a></li>
+                              </ul>
+                           </div>
+                           <h3 class="price_text">Rp 49.900</h3>
+                        </div>
+                     </div>
+                  </div>
+
+                  <div class="seemore_bt"><a href="#">See More</a></div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- product section end -->
+
+      <!-- about section start -->
+      <div id="about" class="about_section layout_padding">
+         <div class="container">
+            <div class="about_section_main">
+               <div class="row">
+                  <div class="col-md-6">
+                     <div class="about_taital_main">
+                        <h1 class="about_taital">About Bukukita.</h1>
+                        <p class="about_text">
+                           Pelayanan yang akan diberikan apabila memilih Bukukita.
+                           <ul>
+                              <li>1. Memberikan Informasi yang Jelas Melalui Gambar dan Deskripsi Produk.</li>
+                              <li>2. Aktif di Media Sosial</li>
+                              <li>3. Memberikan Promo Pada Waktu Tertentu.</li>
+                              <li>4. Memiliki Banyak Testimoni Pelanggan.</li>
+                              <li>5. Menyediakan Layanan Pengiriman yang Cepat.</li>
+                           </ul>
+                        </p>
+                        <div class="readmore_bt"><a href="#">Read More</a></div>
+                     </div>
+                  </div>
+                  <div class="col-md-6">
+                     <div><img src="img/book3.jpg" class="image_3"></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- about section end -->
+
+      <!-- footer section start -->
+      <div class="footer_section layout_padding">
+         <div class="container">
+            <div class="footer_logo"><a href="index.html"><img src="images/footer-logo.png"></a></div>
+            <div class="contact_section_2">
+               <div class="row">
+                  <div class="col-sm-4">
+                     <h3 id="kontak"class="address_text">Contact Us</h3>
+                     <div class="address_bt">
+                        <ul>
+                           <li>
+                              <a href="#">
+                              <i class="fa fa-map-marker" aria-hidden="true"></i><span class="padding_left10">Address : Semarang</span>
+                              </a>
+                           </li>
+                           <li>
+                              <a href="#">
+                              <i class="fa fa-phone" aria-hidden="true"></i><span class="padding_left10">Phone : 085155170678</span>
+                              </a>
+                           </li>
+                           <li>
+                              <a href="#">
+                              <i class="fa fa-envelope" aria-hidden="true"></i><span class="padding_left10">Email : bukukita@gmail.com</span>
+                              </a>
+                           </li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-sm-4">
+                     <div class="footer_logo_1">
+                     <h3 class="address_text">Bukukita.</h3>
+                     </div>
+                     <p class="dummy_text">e-commerce buku yang menawarkan harga terjangkau dengan kualitas yang terbaik</p>
+                  </div>
+                  <div class="col-sm-4">
+                     <div class="main">
+                        <h3 class="address_text">Rekomendasi Produk</h3>
+                        <p class="ipsum_text">dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="social_icon">
+               <ul>
+                  <li>
+                     <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                  </li>
+                  <li>
+                     <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                  </li>
+                  <li>
+                     <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                  </li>
+                  <li>
+                     <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </div>
+      <!-- footer section end -->
+
+      <!-- copyright section start -->
+      <div class="copyright_section">
+         <div class="container">
+            <p class="copyright_text">Yuliana Putri | &copy; 2023</p>
+         </div>
+      </div>
+      <!-- copyright section end -->
+
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <!-- javascript --> 
+      <script src="js/owl.carousel.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>  
+      <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+      <script>
+         function openNav() {
+           document.getElementById("mySidenav").style.width = "100%";
+         }
+         
+         function closeNav() {
+           document.getElementById("mySidenav").style.width = "0";
+         }
+      </script> 
+
+      <script>
+         feather.replace();
+      </script>
+
+      
+   </body>
+</html>
